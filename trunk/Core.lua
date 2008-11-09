407077d8498f6aef4433efcbfd736f29fe3aa2a0
Phanx-28751 <Phanx-28751@c066b7a2-c332-4b50-af3f-84fec4ac175b>
2008-11-09 13:32:08 +0000

    - Reorganize stuff - Update license terms

--- trunk/Core.lua
+++ trunk/Core.lua
@@ -1,19 +1,19 @@
 --[[--------------------------------------------------------------------
-GridStatusHealingReduced
-Adds statuses to Grid for debuffs which reduce or prevent healing received.
-by Phanx < addons AT phanx net >
-http://www.wowinterface.com/downloads/info7364.html
-Please see the included README text file for license terms and additional information.
+	GridStatusHealingReduced
+	Adds statuses to Grid for debuffs which reduce or prevent healing received.
+	by Phanx < addons@phanx.net >
+	http://www.wowinterface.com/downloads/info7364-GridStatusHealingReduced.html
+	See README for license terms and additional information.
 ----------------------------------------------------------------------]]
 
 --[[--------------------------------------------------------------------
-To-Do:
+	To-Do:
 
-Improve debuff scanning efficiency by only looking for debuffs that can
-exist in the player's current location; i.e, don't scan for Kil'jaeden's
-Shadow Spike if the player is in Durotar.
+	Add translations for esES, esMX, and frFR locales.
 
-Add translations for esES, esMX, and frFR locales.
+	Improve debuff scanning efficiency by only looking for debuffs that
+	can be applied in the current zone. i.e. don't scan for Nefarian's
+	Veil of Shadow debuff when the player is in Durotar.
 ----------------------------------------------------------------------]]
 
 local locale = GetLocale()
@@ -38,43 +38,40 @@ setmetatable(L, { __index = function(t, k) rawset(t, k, k); return k; end })
 ----------------------------------------------------------------
 
 local healingReductionDebuffs = {
-	[GetSpellInfo(19434)] = true,	-- 0.50, -- Aimed Shot
-	[GetSpellInfo(40599)] = true,	-- 0.50, -- Arcing Smash (Gurtogg Bloodboil - Black Temple)
---	[GetSpellInfo(23230)] = true,	-- 0.50, -- Blood Fury (Orc racial skill) -- REMOVED in patch 3.0.2
-	[GetSpellInfo(23169)] = true,	-- 0.50, -- Brood Affliction: Green (Chromaggus - Blackwing Lair)
-	[GetSpellInfo(34073)] = true,	-- 0.85, -- Curse of the Bleeding Hollow (Bleeding Hollow orcs - Hellfire Peninsula)
-	[GetSpellInfo(13583)] = true,	-- 0.50, -- Curse of the Deadwood (Deadwood furbolgs - Felwood)
-	[GetSpellInfo(36023)] = true,	-- 0.50, -- Deathblow (Shattered Hand Savage - Shattered Halls)
-	[GetSpellInfo(36054)] = true,	-- 0.50, -- Deathblow (Shattered Hand Savage - Shattered Halls)
-	[GetSpellInfo(34625)] = true,	-- 0.25, -- Demolish (Negatron - Netherstorm)
-	[GetSpellInfo(34366)] = true,	-- 0.75, -- Ebon Poison (Blackfang Tarantula - The Black Morass)
-	[GetSpellInfo(32378)] = true,	-- 0.50, -- Filet (Spectral Chef - Karazhan)
-	[GetSpellInfo(19716)] = true,	-- 0.25, -- Gehennas' Curse (Gehennas - Molten Core)
---	[GetSpellInfo(9035)]  = true,	-- 0.80, -- Hex of Weakness -- REMOVED in patch 3.0.2, at least for priests
-	[GetSpellInfo(36917)] = true,	-- 0.50, -- Magma-Thrower's Curse (Sulfuron Magma-Thrower - The Arcatraz)
-	[GetSpellInfo(22859)] = true,	-- 0.50, -- Mortal Cleave (High Priestess Thekal - Zul'Gurub)
-	[GetSpellInfo(12294)] = true,	-- 0.50, -- Mortal Strike
-	[GetSpellInfo(28776)] = true,	-- 0.10, -- Necrotic Poison (Maexxna - Naxxramas)
-	[GetSpellInfo(45885)] = true, -- 0.50, -- Shadow Spike (Kil'jaeden- Sunwell Plateau)
-	[GetSpellInfo(35189)] = true,	-- 0.50, -- Solar Strike (Bloodwarder Slayer - The Mechanar)
-	[GetSpellInfo(32315)] = true,	-- 0.50, -- Soul Strike (Ethereal Crypt Raider - Mana-Tombs)
-	[GetSpellInfo(7068)]  = true,	-- 0.25, -- Veil of Shadow (Nefarian - Blackwing Lair)
-	[GetSpellInfo(44534)] = true,	-- 0.50, -- Wretched Strike (Wretched Bruiser - Magister's Terrace)
-
-	[GetSpellInfo(32858)] = true,	-- -345, -- Touch of the Forgotten (Auchenai Soulpriest - Auchenai Crypts)
---	[GetSpellInfo(32377)] = true,	-- -690, -- Touch of the Forgotten (Auchenai Soulpriest - Auchenai Crypts) -- DUPLICATE; heroic version of above
-
-	[GetSpellInfo(45347)] = true,	-- 1 - (.04 * 25), -- Dark Touched (Lady Sacrolash - Sunwell Plateau)
-	[GetSpellInfo(30423)] = true,	-- 1 - (.01 * 99), -- Nether Portal - Dominance (Netherspite - Karazhan)
-	[GetSpellInfo(25646)] = true,	-- 1 - (.10 * 7),  -- Mortal Wound (Temporus - The Black Morass)
-	[GetSpellInfo(13218)] = true,	-- 1 - (.10 * 5),  -- Wound Poison
---	[GetSpellInfo(39665)] = true, -- 1 - (.20 * 5),  -- Wound Poison (Hex Lord Malacrass - Zul'Aman) -- DUPLICATE; same name as rogue ability
+	[GetSpellInfo(19434)] = true,		-- Aimed Shot, -50%
+	[GetSpellInfo(40599)] = true,		-- Arcing Smash (Gurtogg Bloodboil - Black Temple), -50%
+--	[GetSpellInfo(23230)] = true,		-- Blood Fury (Orc racial skill), -50%					-- REMOVED in WoW 3.0
+	[GetSpellInfo(23169)] = true,		-- Brood Affliction: Green (Chromaggus - Blackwing Lair), -50%
+	[GetSpellInfo(34073)] = true,		-- Curse of the Bleeding Hollow (Bleeding Hollow orcs - Hellfire Peninsula), -15%
+	[GetSpellInfo(13583)] = true,		-- Curse of the Deadwood (Deadwood furbolgs - Felwood), -50%
+	[GetSpellInfo(45347)] = true,		-- Dark Touched (Lady Sacrolash - Sunwell Plateau), -4% per stack, up to 25
+	[GetSpellInfo(36023)] = true,		-- Deathblow (Shattered Hand Savage - Shattered Halls), -50%
+--	[GetSpellInfo(36054)] = true,		-- Deathblow (Shattered Hand Savage - Shattered Halls), -50%
+	[GetSpellInfo(34625)] = true,		-- Demolish (Negatron - Netherstorm), -75%
+	[GetSpellInfo(34366)] = true,		-- Ebon Poison (Blackfang Tarantula - The Black Morass), -25%
+	[GetSpellInfo(32378)] = true,		-- Filet (Spectral Chef - Karazhan), -50%
+	[GetSpellInfo(19716)] = true,		-- Gehennas' Curse (Gehennas - Molten Core) - 75%
+--	[GetSpellInfo(9035)]  = true,		-- Hex of Weakness, -20%								-- REMOVED in WoW 3.0
+	[GetSpellInfo(36917)] = true,		-- Magma-Thrower's Curse (Sulfuron Magma-Thrower - The Arcatraz), -50%
+	[GetSpellInfo(22859)] = true,		-- Mortal Cleave (High Priestess Thekal - Zul'Gurub), -50%
+	[GetSpellInfo(12294)] = true,		-- Mortal Strike, -50%
+	[GetSpellInfo(25646)] = true,		-- Mortal Wound (Temporus - The Black Morass), -10% per stack, up to 7
+	[GetSpellInfo(28776)] = true,		-- Necrotic Poison (Maexxna - Naxxramas), -90%
+	[GetSpellInfo(30423)] = true,		-- Nether Portal - Dominance (Netherspite - Karazhan), -1% per stack, up to 99
+	[GetSpellInfo(45885)] = true,		-- Shadow Spike (Kil'jaeden - Sunwell Plateau), -50%
+	[GetSpellInfo(35189)] = true,		-- Solar Strike (Bloodwarder Slayer - The Mechanar), -50%
+	[GetSpellInfo(32315)] = true,		-- Soul Strike (Ethereal Crypt Raider - Mana-Tombs), -50%
+	[GetSpellInfo(32858)] = true,		-- Touch of the Forgotten (Auchenai Soulpriest - Auchenai Crypts), -345
+--	[GetSpellInfo(32377)] = true,		-- Touch of the Forgotten (Auchenai Soulpriest - Auchenai Crypts), -690 -- DUPLICATE; heroic version of above
+	[GetSpellInfo(7068)]  = true,		-- Veil of Shadow (Nefarian - Blackwing Lair), -75%
+	[GetSpellInfo(44534)] = true,		-- Wretched Strike (Wretched Bruiser - Magister's Terrace), -50%
+	[GetSpellInfo(13218)] = true,		-- Wound Poison, -10% per stack, up to 5
 }
 
 local healingPreventionDebuffs = {
-	[GetSpellInfo(41292)] = true, -- Aura of Suffering (Essence of Suffering - Black Temple)
-	[GetSpellInfo(45996)] = true, -- Darkness (M'uru - Sunwell Plateau)
-	[GetSpellInfo(30843)] = true, -- Enfeeble (Prince Malchezaar - Karazhan)
+	[GetSpellInfo(41292)] = true,		-- Aura of Suffering (Essence of Suffering - Black Temple)
+	[GetSpellInfo(45996)] = true,		-- Darkness (M'uru - Sunwell Plateau)
+	[GetSpellInfo(30843)] = true,		-- Enfeeble (Prince Malchezaar - Karazhan)
 }
 
 ----------------------------------------------------------------
@@ -82,8 +79,7 @@ local healingPreventionDebuffs = {
 GridStatusHealingReduced = GridStatus:NewModule("GridStatusHealingReduced")
 
 local GridStatusHealingReduced = GridStatusHealingReduced
-local UnitDebuff = UnitDebuff
-local UnitName = UnitName
+local UnitDebuff, UnitName, UnitInParty, UnitInRaid = UnitDebuff, UnitName, UnitInParty, UnitInRaid
 local db
 
 GridStatusHealingReduced.menuName = L["Healing Reduced"]
@@ -119,14 +115,15 @@ function GridStatusHealingReduced:OnEnable()
 	self:RegisterEvent("UNIT_AURA")
 end
 
-local reduced, prevented, name, settings
+local i, reduced, prevented, name, settings
 function GridStatusHealingReduced:UNIT_AURA(unit)
 	if not UnitInRaid(unit) or UnitInParty(unit) then return end
 
 	prevented = false
 	reduced = false
 
-	for i = 1, 40 do
+	i = 1
+	while true do
 		name = UnitDebuff(unit, i)
 		if not name then
 			break
@@ -139,6 +136,7 @@ function GridStatusHealingReduced:UNIT_AURA(unit)
 			reduced = true
 			break
 		end
+		i = i + 1
 	end
 
 	if prevented then
