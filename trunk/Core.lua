0192b37c8f8861b378924b6929b522677ca25576
Phanx-28751 <Phanx-28751@c066b7a2-c332-4b50-af3f-84fec4ac175b>
2008-12-16 11:10:09 +0000

    - Add zone awareness to reduce buff scanning - Change to work with new GUID-based Grid; maintains compatibility with existing name-based version

--- trunk/Core.lua
+++ trunk/Core.lua
@@ -1,86 +1,133 @@
---[[--------------------------------------------------------------------
-	GridStatusHealingReduced
-	Adds statuses to Grid for debuffs which reduce or prevent healing received.
-	by Phanx < addons@phanx.net >
-	http://www.wowinterface.com/downloads/info7364-GridStatusHealingReduced.html
-	See README for license terms and additional information.
-----------------------------------------------------------------------]]
+----------------------------------------------------------------
+--	GridStatusHealingReduced
+--	Adds a Grid status for debuffs that reduce healing taken.
+----------------------------------------------------------------
 
---[[--------------------------------------------------------------------
-	To-Do:
-
-	Add translations for esES, esMX, and frFR locales.
-
-	Improve debuff scanning efficiency by only looking for debuffs that
-	can be applied in the current zone. i.e. don't scan for Nefarian's
-	Veil of Shadow debuff when the player is in Durotar.
-----------------------------------------------------------------------]]
-
-local locale = GetLocale()
-local L = locale == "deDE" and {
-	["Healing Reduced"] = "Heilung reduziert",
-	["Healing Prevented"] = "Heilung verhindert",
-} or locale == "ruRU" and {
-	["Healing Reduced"] = "Исцеление уменьшено",
-	["Healing Prevented"] = "Препятствие исцелению",
-} or locale == "koKR" and {
-	["Healing Reduced"] = "치유량 감소",
-	["Healing Prevented"] = "치유량 방해",
-} or locale == "zhCN" and {
-	["Healing Reduced"] = "治疗效果被降低",
-	["Healing Prevented"] = "治疗无效果",
-} or locale == "zhTW" and {
-	["Healing Reduced"] = "治療效果被降低",
-	["Healing Prevented"] = "治療無效果",
-} or {}
-setmetatable(L, { __index = function(t, k) rawset(t, k, k); return k; end })
+local L = {}
+do
+	local locale = GetLocale()
+	if locale ~= "enUS" and locale ~= "enGB" then
+		if locale == "deDE" then
+			L["Healing Reduced"] = "Heilung reduziert"
+			L["Healing Prevented"] = "Heilung verhindert"
+	--	elseif locale == "esES" then
+	--		L["Healing Reduced"] = ""
+	--		L["Healing Prevented"] = ""
+	--	elseif locale == "esMX" then
+	--		L["Healing Reduced"] = ""
+	--		L["Healing Prevented"] = ""
+	--	elseif locale == "frFR" then
+	--		L["Healing Reduced"] = ""
+	--		L["Healing Prevented"] = ""
+		elseif locale == "ruRU" then
+			L["Healing Reduced"] = "Исцеление уменьшено"
+			L["Healing Prevented"] = "Препятствие исцелению"
+		elseif locale == "koKR" then
+			L["Healing Reduced"] = "치유량 감소"
+			L["Healing Prevented"] = "치유량 방해"
+		elseif locale == "zhCN" then
+			L["Healing Reduced"] = "治疗效果被降低"
+			L["Healing Prevented"] = "治疗无效果"
+		elseif locale == "zhTW" then
+			L["Healing Reduced"] = "治療效果被降低"
+			L["Healing Prevented"] = "治療無效果"
+		end
+		for english, localized in pairs(LibStub("LibBabble-Zone-3.0"):GetLookupTable()) do
+			L[english] = localized
+		end
+	end
+	setmetatable(L, { __index = function(t, k) rawset(t, k, k) return k end })
+end
 
 ----------------------------------------------------------------
 
-local healingReductionDebuffs = {
-	[GetSpellInfo(19434)] = true,		-- Aimed Shot, -50%
-	[GetSpellInfo(40599)] = true,		-- Arcing Smash (Gurtogg Bloodboil - Black Temple), -50%
---	[GetSpellInfo(23230)] = true,		-- Blood Fury (Orc racial skill), -50%					-- REMOVED in WoW 3.0
-	[GetSpellInfo(23169)] = true,		-- Brood Affliction: Green (Chromaggus - Blackwing Lair), -50%
-	[GetSpellInfo(34073)] = true,		-- Curse of the Bleeding Hollow (Bleeding Hollow orcs - Hellfire Peninsula), -15%
-	[GetSpellInfo(13583)] = true,		-- Curse of the Deadwood (Deadwood furbolgs - Felwood), -50%
-	[GetSpellInfo(45347)] = true,		-- Dark Touched (Lady Sacrolash - Sunwell Plateau), -4% per stack, up to 25
-	[GetSpellInfo(36023)] = true,		-- Deathblow (Shattered Hand Savage - Shattered Halls), -50%
---	[GetSpellInfo(36054)] = true,		-- Deathblow (Shattered Hand Savage - Shattered Halls), -50%
-	[GetSpellInfo(34625)] = true,		-- Demolish (Negatron - Netherstorm), -75%
-	[GetSpellInfo(34366)] = true,		-- Ebon Poison (Blackfang Tarantula - The Black Morass), -25%
-	[GetSpellInfo(32378)] = true,		-- Filet (Spectral Chef - Karazhan), -50%
-	[GetSpellInfo(19716)] = true,		-- Gehennas' Curse (Gehennas - Molten Core) - 75%
---	[GetSpellInfo(9035)]  = true,		-- Hex of Weakness, -20%								-- REMOVED in WoW 3.0
-	[GetSpellInfo(36917)] = true,		-- Magma-Thrower's Curse (Sulfuron Magma-Thrower - The Arcatraz), -50%
-	[GetSpellInfo(22859)] = true,		-- Mortal Cleave (High Priestess Thekal - Zul'Gurub), -50%
-	[GetSpellInfo(12294)] = true,		-- Mortal Strike, -50%
-	[GetSpellInfo(25646)] = true,		-- Mortal Wound (Temporus - The Black Morass), -10% per stack, up to 7
-	[GetSpellInfo(28776)] = true,		-- Necrotic Poison (Maexxna - Naxxramas), -90%
-	[GetSpellInfo(30423)] = true,		-- Nether Portal - Dominance (Netherspite - Karazhan), -1% per stack, up to 99
-	[GetSpellInfo(45885)] = true,		-- Shadow Spike (Kil'jaeden - Sunwell Plateau), -50%
-	[GetSpellInfo(35189)] = true,		-- Solar Strike (Bloodwarder Slayer - The Mechanar), -50%
-	[GetSpellInfo(32315)] = true,		-- Soul Strike (Ethereal Crypt Raider - Mana-Tombs), -50%
-	[GetSpellInfo(32858)] = true,		-- Touch of the Forgotten (Auchenai Soulpriest - Auchenai Crypts), -345
---	[GetSpellInfo(32377)] = true,		-- Touch of the Forgotten (Auchenai Soulpriest - Auchenai Crypts), -690 -- DUPLICATE; heroic version of above
-	[GetSpellInfo(7068)]  = true,		-- Veil of Shadow (Nefarian - Blackwing Lair), -75%
-	[GetSpellInfo(44534)] = true,		-- Wretched Strike (Wretched Bruiser - Magister's Terrace), -50%
-	[GetSpellInfo(13218)] = true,		-- Wound Poison, -10% per stack, up to 5
+local reductionDebuffs = {
+	["*"] = {
+		[GetSpellInfo(19434)] = true,	-- 0.50, -- Aimed Shot
+--		[GetSpellInfo(23230)] = true,	-- 0.50, -- Blood Fury -- debuff component REMOVED in WoW 3.0
+--		[GetSpellInfo(9035)]  = true,	-- 0.80, -- Hex of Weakness -- priest racial REMOVED in WoW 3.0
+		[GetSpellInfo(12294)] = true,	-- 0.50, -- Mortal Strike
+		[GetSpellInfo(13218)] = true,	-- 1 - (.10 * 5),  -- Wound Poison
+	},
+	[L["Auchenai Crypts"]] = {
+		[GetSpellInfo(32858)] = true,	-- -345, -- Touch of the Forgotten (Auchenai Soulpriest)
+--		[GetSpellInfo(32377)] = true,	-- -690, -- Touch of the Forgotten (Auchenai Soulpriest)
+	},
+	[L["Black Temple"]] = {
+		[GetSpellInfo(40599)] = true,	-- 0.50, -- Arcing Smash (Gurtogg Bloodboil)
+	},
+	[L["Blackwing Lair"]] = {
+		[GetSpellInfo(23169)] = true,	-- 0.50, -- Brood Affliction: Green (Chromaggus)
+		[GetSpellInfo(7068)]  = true,	-- 0.25, -- Veil of Shadow (Nefarian)
+	},
+	[L["Felwood"]] = {
+		[GetSpellInfo(13583)] = true,	-- 0.50, -- Curse of the Deadwood (Deadwood furbolgs)
+	},
+	[L["Hellfire Peninsula"]] = {
+		[GetSpellInfo(34073)] = true,	-- 0.85, -- Curse of the Bleeding Hollow (Bleeding Hollow orcs)
+	},
+	[L["Karazhan"]] = {
+		[GetSpellInfo(32378)] = true,	-- 0.50, -- Filet (Spectral Chef)
+		[GetSpellInfo(30423)] = true,	-- 1 - (.01 * 99), -- Nether Portal - Dominance (Netherspite)
+	},
+	[L["Magister's Terrace"]] = {
+		[GetSpellInfo(44534)] = true,	-- 0.50, -- Wretched Strike (Wretched Bruiser)
+	},
+	[L["Mana-Tombs"]] = {
+		[GetSpellInfo(32315)] = true,	-- 0.50, -- Soul Strike (Ethereal Crypt Raider)
+	},
+	[L["Molten Core"]] = {
+		[GetSpellInfo(19716)] = true,	-- 0.25, -- Gehennas' Curse (Gehennas)
+	},
+	[L["Naxxramas"]] = {
+		[GetSpellInfo(28776)] = true,	-- 0.10, -- Necrotic Poison (Maexxna)
+	},
+	[L["Netherstorm"]] = {
+		[GetSpellInfo(34625)] = true,	-- 0.25, -- Demolish (Negatron)
+	},
+	[L["Shattered Halls"]] = {
+		[GetSpellInfo(36023)] = true,	-- 0.50, -- Deathblow (Shattered Hand Savage)
+		[GetSpellInfo(36054)] = true,	-- 0.50, -- Deathblow (Shattered Hand Savage)
+	},
+	[L["Sunwell Plateau"]] = {
+		[GetSpellInfo(45885)] = true, -- 0.50, -- Shadow Spike (Kil'jaeden)
+		[GetSpellInfo(45347)] = true,	-- 1 - (.04 * 25), -- Dark Touched (Lady Sacrolash)
+	},
+	[L["The Arcatraz"]] = {
+		[GetSpellInfo(36917)] = true,	-- 0.50, -- Magma-Thrower's Curse (Sulfuron Magma-Thrower)
+	},
+	[L["The Black Morass"]] = {
+		[GetSpellInfo(34366)] = true,	-- 0.75, -- Ebon Poison (Blackfang Tarantula)
+		[GetSpellInfo(25646)] = true,	-- 1 - (.10 * 7),  -- Mortal Wound (Temporuss)
+	},
+	[L["The Mechanar"]] = {
+		[GetSpellInfo(35189)] = true,	-- 0.50, -- Solar Strike (Bloodwarder Slayer)
+	},
+	[L["Zul'Gurub"]] = {
+		[GetSpellInfo(22859)] = true,	-- 0.50, -- Mortal Cleave (High Priestess Thekal)
+	},
 }
 
-local healingPreventionDebuffs = {
-	[GetSpellInfo(41292)] = true,		-- Aura of Suffering (Essence of Suffering - Black Temple)
-	[GetSpellInfo(45996)] = true,		-- Darkness (M'uru - Sunwell Plateau)
-	[GetSpellInfo(30843)] = true,		-- Enfeeble (Prince Malchezaar - Karazhan)
+local preventionDebuffs = {
+	[L["Black Temple"]] = {
+		[GetSpellInfo(41292)] = true, -- Aura of Suffering (Essence of Suffering - Black Temple)
+	},
+	[L["Karazhan"]] = {
+		[GetSpellInfo(30843)] = true, -- Enfeeble (Prince Malchezaar - Karazhan)
+	},
+	[L["Sunwell Plateau"]] = {
+		[GetSpellInfo(45996)] = true, -- Darkness (M'uru - Sunwell Plateau)
+	},
 }
 
 ----------------------------------------------------------------
 
-GridStatusHealingReduced = GridStatus:NewModule("GridStatusHealingReduced")
+local GridStatusHealingReduced = GridStatus:NewModule("GridStatusHealingReduced")
 
-local GridStatusHealingReduced = GridStatusHealingReduced
-local UnitDebuff, UnitName, UnitInParty, UnitInRaid = UnitDebuff, UnitName, UnitInParty, UnitInRaid
-local db
+local UnitAura = UnitAura
+local UnitGUID = Grid:HasModule("GridRoster") and UnitGUID or UnitName
+
+local reduction_debuffs, prevention_debuffs, reduced_units, prevented_units, valid_units, db = {}, {}, {}, {}
 
 GridStatusHealingReduced.menuName = L["Healing Reduced"]
 GridStatusHealingReduced.options = false
@@ -109,46 +156,98 @@ function GridStatusHealingReduced:OnInitialize()
 	self:RegisterStatus("alert_healingPrevented", L["Healing Prevented"], nil, true)
 
 	db = self.db.profile
+
+	self.raid_units = {}
+	for i = 1, 40 do
+		self.raid_units["raid"..i] = true
+		self.raid_units["raidpet"..i] = true
+	end
+
+	self.party_units = {
+		player = true,
+		pet = true,
+	}
+	for i = 1, 4 do
+		self.party_units["party"..i] = true
+		self.party_units["partypet"..i] = true
+	end
 end
 
 function GridStatusHealingReduced:OnEnable()
 	self:RegisterEvent("UNIT_AURA")
+	self:RegisterEvent("ZONE_CHANGED_NEW_AREA")
+	self:RegisterEvent("RAID_ROSTER_UPDATE")
+	self:RegisterEvent("PARTY_MEMBERS_CHANGED", "RAID_ROSTER_UPDATE")
+
+	self:RAID_ROSTER_UPDATE()
+	self:ZONE_CHANGED_NEW_AREA()
 end
 
-local i, reduced, prevented, name, settings
+function GridStatusHealingReduced:RAID_ROSTER_UPDATE()
+	if GetNumRaidMembers() > 0 then
+		valid_units = self.raid_units
+	else
+		valid_units = self.party_units
+	end
+end
+
+function GridStatusHealingReduced:ZONE_CHANGED_NEW_AREA()
+	wipe(reduction_debuffs)
+	wipe(prevention_debuffs)
+
+	for debuff in pairs(reductionDebuffs["*"]) do
+		reduction_debuffs[debuff] = true
+	end
+
+	if reductionDebuffs[GetRealZoneText()] then
+		for debuff in pairs(reductionDebuffs[GetRealZoneText()]) do
+			reduction_debuffs[debuff] = true
+		end
+	end
+
+	if preventionDebuffs[GetRealZoneText()] then
+		for debuff in pairs(preventionDebuffs[GetRealZoneText()]) do
+			prevention_debuffs[debuff] = true
+		end
+	end
+end
+
+local reduced, prevented, settings
 function GridStatusHealingReduced:UNIT_AURA(unit)
-	if not UnitInRaid(unit) or UnitInParty(unit) then return end
+	if not valid_units[unit] then return end
 
 	prevented = false
 	reduced = false
 
-	i = 1
-	while true do
-		name = UnitDebuff(unit, i)
-		if not name then
-			break
-		end
-		if healingPreventionDebuffs[name] then
+	for debuff in pairs(prevention_debuffs) do
+		if UnitAura(unit, debuff) then
 			prevented = true
 			break
 		end
-		if healingReductionDebuffs[name] then
+	end
+
+	if prevented and not prevented_units[unit] then
+		prevented_units[unit] = true
+		settings = db.alert_healingPrevented
+		self.core:SendStatusGained(UnitGUID(unit), "alert_healingPrevented", settings.priority, (settings.range and 40), settings.color, settings.text)
+	elseif prevented_units[unit] then
+		prevented_units[unit] = false
+		self.core:SendStatusLost(UnitGUID(unit), "alert_healingPrevented")
+	end
+
+	for debuff in pairs(reduction_debuffs) do
+		if UnitAura(unit, debuff) then
 			reduced = true
 			break
 		end
-		i = i + 1
 	end
 
-	if prevented then
-		settings = db.alert_healingPrevented
-		self.core:SendStatusGained(UnitName(unit), "alert_healingPrevented", settings.priority, (settings.range and 40), settings.color, settings.text)
-	else
-		self.core:SendStatusLost(UnitName(unit), "alert_healingPrevented")
-	end
-	if reduced then
+	if reduced and not reduced_units[units] then
+		reduced_units[unit] = true
 		settings = db.alert_healingReduced
-		self.core:SendStatusGained(UnitName(unit), "alert_healingReduced", settings.priority, (settings.range and 40), settings.color, settings.text)
-	else
-		self.core:SendStatusLost(UnitName(unit), "alert_healingReduced")
+		self.core:SendStatusGained(UnitGUID(unit), "alert_healingReduced", settings.priority, (settings.range and 40), settings.color, settings.text)
+	elseif reduced_units[unit] then
+		reduced_units[unit] = false
+		self.core:SendStatusLost(UnitGUID(unit), "alert_healingReduced")
 	end
 end
