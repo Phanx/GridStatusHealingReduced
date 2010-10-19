f34c8273ca2825a9c25cd5bdd2ab5e3fbd6b7601
Phanx <addons@phanx.net>
2010-10-19 20:31:27 +0000

    * Debuffs are now recognized by spell ID, rather than spell name. * All debuffs cast by NPCs outside of instances have been removed:     * Chop (Dragonblight, Grizzly Hills, Howling Fjord, Hrothgar's Landing, Icecrown)     * Curse of the Bleeding Hollow (Hellfire Peninusla)     * Curse of the Deadwood (Felwood)     * Demolish (Netherstorm)     * Filet (Nagrand)     * Hex of Weakness (Zul'Drak)     * Necrotic Strike (Icecrown) * All debuffs cast in level 60 instances have been removed:     * Brood Affliction: Green (Blackwing Lair)     * Gehennas' Curse (Molten Core     * Mortal Cleave (Dire Maul)     * Veil of Shadow (Blackwing Lair) * All debuffs cast in level 70 instances have been removed:     * Arcing Smash (Gruul's Lair)     * Aura of Suffering (Black Temple)     * Dark Touched (Sunwell Plateau)     * Darkness (Sunwell Plateau)     * Deathblow (The Shattered Halls)     * Ebon Poison (Black Morass)     * Enfeeble (Karazhan)     * Magma-Thrower's Curse (The Arcatraz)     * Mortal Cleave (Serpentshrine Cavern)     * Nether Portal - Dominance (Karazhan)     * Shadow Spike (Sunwell Plateau)     * Solar Strike (The Mechanar)     * Touch of the Forgotten (Auchenai Crypts)     * Wound Poison (Hex Lord Malacrass)     * Wretched Strike (Magisters' Terrace)

--- Core.lua
+++ Core.lua
@@ -2,7 +2,7 @@
 	GridStatusHealingReduced
 	Adds aggregate statuses to Grid for debuffs which reduce or prevent healing received.
 	by Phanx < addons@phanx.net >
-	Copyright © 2008–2010 Alyssa "Phanx" Kinley
+	Copyright © 2008–2010 Phanx.
 	See README for license terms and additional information.
 	http://www.wowinterface.com/downloads/info7364-GridStatusHealingReduced.html
 	http://wow.curse.com/downloads/wow-addons/details/gridstatushealingreduced.aspx
@@ -38,90 +38,16 @@ end
 
 ------------------------------------------------------------------------
 
-local function GetSpellName(id)
-	local name = GetSpellInfo(id)
-	return name or ""
-end
-
--- Debuffs which reduce healing received
-local ReductionDebuffs = {
-	[GetSpellName(19434)] = true, -- Aimed Shot
-	[GetSpellName(40599)] = true, -- Arcing Smash
-	[GetSpellName(23169)] = true, -- Brood Affliction: Green
-	[GetSpellName(43410)] = true, -- Chop
-	[GetSpellName(34073)] = true, -- Curse of the Bleeding Hollow
-	[GetSpellName(13583)] = true, -- Curse of the Deadwood
-	[GetSpellName(45347)] = true, -- Dark Touched
-	[GetSpellName(63038)] = true, -- Dark Volley
-	[GetSpellName(36023)] = true, -- Deathblow
-	[GetSpellName(34625)] = true, -- Demolish
-	[GetSpellName(34366)] = true, -- Ebon Poison
-	[GetSpellName(48291)] = true, -- Fetid Rot
-	[GetSpellName(32378)] = true, -- Filet
-	[GetSpellName(56112)] = true, -- Furious Attacks
-	[GetSpellName(19716)] = true, -- Gehennas' Curse
-	[GetSpellName(52645)] = true, -- Hex of Weakness
-	[GetSpellName(70671)] = true, -- Leeching Rot -- 70710 Heroic
-	[GetSpellName(36917)] = true, -- Magma-Thrower's Curse
-	[GetSpellName(48301)] = true, -- Mind Trauma
-	[GetSpellName(22859)] = true, -- Mortal Cleave
-	[GetSpellName(12294)] = true, -- Mortal Strike (Warriors)
-	[GetSpellName(24573)] = true, -- Mortal Strike (Broodlord Lashlayer)
-	[GetSpellName(43441)] = true, -- Mortal Strike (Hex Lord Malacrass)
-	[GetSpellName(44268)] = true, -- Mortal Strike (Warlord Salaris)
-	[GetSpellName(25646)] = true, -- Mortal Wound
-	[GetSpellName(31464)] = true, -- Mortal Wound
-	[GetSpellName(36814)] = true, -- Mortal Wound
-	[GetSpellName(54378)] = true, -- Mortal Wound
-	[GetSpellName(69674)] = true, -- Mutated Infection
-	[GetSpellName(28776)] = true, -- Necrotic Poison
-	[GetSpellName(60626)] = true, -- Necrotic Strike
-	[GetSpellName(30423)] = true, -- Nether Portal - Dominance
-	[GetSpellName(68391)] = true, -- Permafrost
-	[GetSpellName(59525)] = true, -- Ray of Pain
-	[GetSpellName(45885)] = true, -- Shadow Spike
-	[GetSpellName(54525)] = true, -- Shroud of Darkness
-	[GetSpellName(35189)] = true, -- Solar Strike
-	[GetSpellName(32315)] = true, -- Soul Strike
-	[GetSpellName(70588)] = true, -- Suppression
-	[GetSpellName(32858)] = true, -- Touch of the Forgotten
-	[GetSpellName(7068)]  = true, -- Veil of Shadow (Nefarian)
-	[GetSpellName(28440)] = true, -- Veil of Shadow (Dread Creeper)
-	[GetSpellName(69633)] = true, -- Veil of Shadow (Spectral Warden)
-	[GetSpellName(13218)] = true, -- Wound Poison (Rogues)
-	[GetSpellName(43461)] = true, -- Wound Poison (Hex Lord Malacrass)
-	[GetSpellName(13222)] = true, -- Wound Poison II
-	[GetSpellName(13223)] = true, -- Wound Poison III
-	[GetSpellName(13224)] = true, -- Wound Poison IV
-	[GetSpellName(27189)] = true, -- Wound Poison V
-	[GetSpellName(57974)] = true, -- Wound Poison VI
-	[GetSpellName(57975)] = true, -- Wound Poison VII
-	[GetSpellName(52771)] = true, -- Wounding Strike
-	[GetSpellName(44534)] = true, -- Wretched Strike
-}
-
--- Debuffs which prevent healing received
-local PreventionDebuffs = {
-	[GetSpellName(41292)] = true, -- Aura of Suffering
-	[GetSpellName(45996)] = true, -- Darkness
-	[GetSpellName(59513)] = true, -- Embrace of the Vampyr
-	[GetSpellName(30843)] = true, -- Enfeeble
-	[GetSpellName(55593)] = true, -- Necrotic Aura
-}
-
-------------------------------------------------------------------------
+local ReductionDebuffs
+local PreventionDebuffs
 
 local UnitDebuff = UnitDebuff
 local UnitGUID = UnitGUID
 
 local enabled = 0
 
-local function debug(str)
-	print("|cffff9933GridStatusHealingReduced:|r " .. str)
-end
-
 local valid = { player = true, pet = true, vehicle = true }
-for i = 1, 5 do
+for i = 1, 4 do
 	valid["party" .. i] = true
 	valid["partypet" .. i] = true
 end
@@ -129,6 +55,12 @@ for i = 1, 40 do
 	valid["raid" .. i] = true
 	valid["raidpet" .. i] = true
 end
+
+local function debug(str)
+	print("|cffff9933GridStatusHealingReduced:|r " .. str)
+end
+
+------------------------------------------------------------------------
 
 local GridStatusHealingReduced = Grid:GetModule("GridStatus"):NewModule("GridStatusHealingReduced")
 
@@ -157,6 +89,22 @@ function GridStatusHealingReduced:OnInitialize()
 --	debug("OnInitialize")
 	self.super.OnInitialize(self)
 
+	ReductionDebuffs = { }
+	for id in string.split(",", "48871,59243,65883,67977,67978,67979,63038,48291,56112,70671,70710,30213,48301,12294,13737,16856,32736,35054,39171,57789,65926,67542,68782,68783,68784,71552,28467,48187,59265,54378,71127,69674,28776,68391,59525,54525,32315,70588,28440,53803,69633,82654,13218,13222,13223,13224,27189,57974,57975,52771") do
+		local name = GetSpellInfo(id)
+		if name then
+			ReductionDebuffs[id] = true
+		end
+	end
+
+	PreventionDebuffs = { }
+	for id in string.split(",", "59513,55593") do
+		local name = GetSpellInfo(id)
+		if name then
+			PreventionDebuffs[id] = true
+		end
+	end
+
 	self:RegisterStatus("alert_healingReduced", HEALING_REDUCED, nil, true)
 	self:RegisterStatus("alert_healingPrevented", HEALING_PREVENTED, nil, true)
 end
@@ -169,6 +117,8 @@ end
 function GridStatusHealingReduced:OnStatusEnable(status)
 --	debug("OnStatusEnable, " .. status)
 
+	if status ~= "alert_healingReduced" and status ~= "alert_healingPrevented" then return end
+
 	enabled = enabled + 1
 	self:RegisterEvent("UNIT_AURA", "UpdateUnit")
 	self:UpdateAllUnits()
@@ -176,6 +126,8 @@ end
 
 function GridStatusHealingReduced:OnStatusDisable(status)
 --	debug("OnStatusDisable, " .. status)
+
+	if status ~= "alert_healingReduced" and status ~= "alert_healingPrevented" then return end
 
 	enabled = enabled - 1
 	if enabled == 0 then
@@ -198,30 +150,39 @@ function GridStatusHealingReduced:UpdateUnit(event, unit)
 	if not valid[unit] then return end
 --	debug("UNIT_AURA, " .. unit)
 
-	local i = 1
-	local prevented, reduced
-	local settings, name, icon, count, duration, expirationTime, _
-	while true do
-		name, _, icon, count, _, duration, expirationTime = UnitDebuff(unit, i)
-		if not name then
+	local prevented, reduced, name, icon, count, duration, expiration, id, _
+	for i = 1, 100 do
+		name, _, icon, count, _, duration, expirationTime, _, _, _, id = UnitDebuff(unit, i)
+		if not id then
 			break
 		end
 		if duration then
-			if PreventionDebuffs[name] then
+			if PreventionDebuffs[id] then
+			--	debug(id .. " " .. name)
 				prevented = true
 				reduced = false
 				break
-			elseif not reduced and ReductionDebuffs[name] then
+			elseif not reduced and ReductionDebuffs[id] then
+			--	debug(id .. " " .. name)
 				reduced = true
 			end
 		end
-		i = i + 1
 	end
 
+	local settings
 	if prevented then
 	--	debug("SendStatusGained")
 		settings = self.db.profile.alert_healingPrevented
-		self.core:SendStatusGained(UnitGUID(unit), "alert_healingPrevented", settings.priority, (settings.range and 40), settings.color, settings.text, nil, nil, icon, expirationTime and (expirationTime - duration), count)
+		self.core:SendStatusGained(UnitGUID(unit), "alert_healingPrevented",
+			settings.priority,
+			(settings.range and 40),
+			settings.color,
+			settings.text,
+			nil,
+			nil,
+			icon,
+			expiration and (expiration - duration),
+			count)
 	else
 	--	debug("SendStatusLost")
 		self.core:SendStatusLost(UnitGUID(unit), "alert_healingPrevented")
@@ -230,7 +191,16 @@ function GridStatusHealingReduced:UpdateUnit(event, unit)
 	if reduced then
 	--	debug("SendStatusGained")
 		settings = self.db.profile.alert_healingReduced
-		self.core:SendStatusGained(UnitGUID(unit), "alert_healingReduced", settings.priority, (settings.range and 40), settings.color, settings.text, nil, nil, icon, expirationTime and (expirationTime - duration), count)
+		self.core:SendStatusGained(UnitGUID(unit), "alert_healingReduced",
+			settings.priority,
+			(settings.range and 40),
+			settings.color,
+			settings.text,
+			nil,
+			nil,
+			icon,
+			expiration and (expiration - duration),
+			count)
 	else
 	--	debug("SendStatusLost")
 		self.core:SendStatusLost(UnitGUID(unit), "alert_healingReduced")
