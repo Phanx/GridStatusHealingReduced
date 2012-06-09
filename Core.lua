a5083dc009f0996c1f5663b489110e0a2cb7cddb
Phanx <addons@phanx.net>
2012-06-09 13:03:03 +0000

    - Added Aura of Deth'spair [101497] Deth'tilac in Molten Front - Added Blistering Wound [100526] Flamewalker Subjugator in Firelands - Added Dampening [110310] - Added Legion Strike [109388] - Added Mortal Strike [103002] Twilight Bruiser in Hour of Twilight - Added Rotting Bile [91099] Corpse Eater in Shadowfang Keep (Heroic) - Added Suffocating Prey [101340] Kirix in Molten Front - Added The Widow's Kiss [99476] Beth'tilac in Firelands - Updated copyright date

--- Core.lua
+++ Core.lua
@@ -1,9 +1,8 @@
 --[[--------------------------------------------------------------------
 	GridStatusHealingReduced
-	Adds generic statuses to Grid for debuffs which reduce or prevent healing received.
-	Written by Phanx <addons@phanx.net>
-	Maintained by Akkorian <akkorian@hotmail.com>
-	Copyright © 2008–2011 Phanx. Some rights reserved. See LICENSE.txt for details.
+	Grid plugin to show debuffs which reduce or prevent healing received.
+	by Phanx <addons@phanx.net>
+	Copyright © 2008–2012 Phanx. Some rights reserved. See LICENSE.txt for details.
 	http://www.wowinterface.com/downloads/info7364-GridStatusHealingReduced.html
 	http://www.curse.com/addons/wow/gridstatushealingreduced
 ----------------------------------------------------------------------]]
@@ -50,31 +49,42 @@ end
 ------------------------------------------------------------------------
 
 local ReductionDebuffs = {
-	-- [[ Classic ]]
+	--[[ Classic ]]
 	23169,13583,19716,22859,13737,24573,27580,25646,31464,17820,22687,
 	78841,43410,19643,32736,
-	-- [[ The Burning Crusade ]]
-	56112,36054,30213,48301,54680,12294,82654,13218,23169,13583,19716,22859,13737,24573,27580,25646,31464,17820,22687,40599,41478,45347,36023,36917,37335,44268,30641,36814,30423,45885,35189,32858,38377,44534,
-	34073,34366,32378,17547,38770,
-	-- [[ Wrath of the Lich King ]]
-	48871,59243,65883,67977,67978,67979,63038,48291,59300,70671,70710,15708,16856,35054,57789,65926,67542,68782,68783,68784,71552,28467,48137,54378,59265,71127,69674,71224,73022,73023,28776,54121,59525,54525,59745,59746,32315,28440,53803,69633,69651,72569,72570,72571,
+	--[[ The Burning Crusade ]]
+	56112,36054,30213,48301,54680,12294,82654,13218,13583,19716,
+	22859,13737,24573,27580,25646,31464,17820,22687,40599,41478,45347,
+	36023,36917,37335,44268,30641,36814,30423,45885,35189,32858,38377,
+	44534,34073,34366,32378,17547,38770,
+	--[[ Wrath of the Lich King ]]
+	48871,59243,65883,67977,67978,67979,63038,48291,59300,70671,70710,
+	15708,16856,35054,57789,65926,67542,68782,68783,68784,71552,28467,
+	48137,54378,59265,71127,69674,71224,73022,73023,28776,54121,59525,
+	54525,59745,59746,32315,28440,53803,69633,69651,72569,72570,72571,
 	54615,52645,44475,80390,60626,
-	-- [[ Cataclysm ]]
-	76189,93956,83908,86157,86158,86158,76727,90526,91801,93675,93771,7068,23224,83926,52771,58830,75571,93452,99506,
-	86816,
-	-- [[ Players ]]
+	--[[ Cataclysm ]]
+	76189,93956,83908,86157,86158,86159,76727,90526,91801,93675,93771,
+	7068,23224,83926,52771,58830,75571,93452,99506,86816,100526,103002,
+	--[[ Cataclysm Solo ]]
+	83930,101340
+	--[[ Players ]]
 	56112,30213,48301,54680,12294,82654,13218,
-	-- [[ Unknown ]]
-	23230,95353,34625,95437,38572,39595,61042,29572,31911,40220,43441,95410,54715,54716,59454,59455,36693,46296,68391,60084,68881,24674,30984,36974,39665,43461,54074,65962,
+	--[[ Unknown ]]
+	23230,95353,34625,95437,38572,39595,61042,29572,31911,40220,43441,
+	95410,54715,54716,59454,59455,36693,46296,68391,60084,68881,24674,
+	30984,36974,39665,43461,54074,65962,109388,110310,91099,99476
 }
 
 local PreventionDebuffs = {
-	-- [[ The Burning Crusade ]]
-	41292,42071,30843,
-	-- [[ Wrath of the Lich King ]]
+	--[[ The Burning Crusade ]]
+	41292,42017,30843,
+	--[[ Wrath of the Lich King ]]
 	59513,55593,
-	-- [[ Cataclysm ]]
+	--[[ Cataclysm ]]
 	76903,82170,92787,92981,92982,92983,82890,85576,93181,93182,93183,
+	--[[ Cataclysm Solo ]]
+	101497,
 }
 
 ------------------------------------------------------------------------
@@ -85,17 +95,13 @@ local UnitGUID = UnitGUID
 local enabled = 0
 
 local valid = { player = true, pet = true, vehicle = true }
-for i = 1, 4 do
+for i = 1, MAX_PARTY_MEMBERS do
 	valid["party" .. i] = true
 	valid["partypet" .. i] = true
 end
-for i = 1, 40 do
+for i = 1, MAX_RAID_MEMBERS do
 	valid["raid" .. i] = true
 	valid["raidpet" .. i] = true
-end
-
-local function debug(str)
-	print("|cffff9933GridStatusHealingReduced:|r " .. str)
 end
 
 ------------------------------------------------------------------------
@@ -119,21 +125,20 @@ GridStatusHealingReduced.defaultDB = {
 
 ------------------------------------------------------------------------
 
-function GridStatusHealingReduced:OnInitialize()
---	debug("OnInitialize")
-	self.super.OnInitialize(self)
+function GridStatusHealingReduced:PostInitialize()
+	self:Debug("PostInitialize")
 
 	local RD, PD = { }, { }
 	for _, id in ipairs(ReductionDebuffs) do
 		local name = GetSpellInfo(id)
 		if name then
-			RD[id] = true
+			RD[id] = name
 		end
 	end
 	for _, id in ipairs(PreventionDebuffs) do
 		local name = GetSpellInfo(id)
 		if name then
-			PD[id] = true
+			PD[id] = name
 		end
 	end
 	ReductionDebuffs, PreventionDebuffs = RD, PD
@@ -142,23 +147,20 @@ function GridStatusHealingReduced:OnInitialize()
 	self:RegisterStatus("alert_healingPrevented", L["Healing prevented"], nil, true)
 end
 
-function GridStatusHealingReduced:OnEnable()
---	debug("OnEnable")
-	self.super.OnEnable(self)
-end
-
 function GridStatusHealingReduced:OnStatusEnable(status)
---	debug("OnStatusEnable, " .. status)
+	self:Debug("OnStatusEnable", status)
 
 	if status ~= "alert_healingReduced" and status ~= "alert_healingPrevented" then return end
 
 	enabled = enabled + 1
-	self:RegisterEvent("UNIT_AURA", "UpdateUnit")
+	if enabled == 0 then
+		self:RegisterEvent("UNIT_AURA", "UpdateUnit")
+	end
 	self:UpdateAllUnits()
 end
 
 function GridStatusHealingReduced:OnStatusDisable(status)
---	debug("OnStatusDisable, " .. status)
+	self:Debug("OnStatusDisable", status)
 
 	if status ~= "alert_healingReduced" and status ~= "alert_healingPrevented" then return end
 
@@ -171,32 +173,23 @@ end
 
 ------------------------------------------------------------------------
 
-function GridStatusHealingReduced:UpdateAllUnits()
-	if enabled > 0 then
-		for guid, unitid in Grid:GetModule("GridRoster"):IterateRoster() do
-			self:UpdateUnit("UpdateAllUnits", unitid)
-		end
-	end
-end
-
 function GridStatusHealingReduced:UpdateUnit(event, unit)
 	if not valid[unit] then return end
---	debug("UNIT_AURA, " .. unit)
+	--self:Debug("UpdateUnit", event, unit)
 
 	local prevented, reduced, name, icon, count, duration, expiration, id, _
-	for i = 1, 100 do
+	for i = 1, 40 do
 		name, _, icon, count, _, duration, expiration, _, _, _, id = UnitDebuff(unit, i)
 		if not id then
 			break
 		end
 		if duration then
 			if PreventionDebuffs[id] then
-			--	debug(id .. " " .. name)
+				self:Debug("Found prevention debuff:", unit, id, name)
 				prevented = true
-				reduced = false
 				break
 			elseif not reduced and ReductionDebuffs[id] then
-			--	debug(id .. " " .. name)
+				self:Debug("Found reduction debuff:", unit, id, name)
 				reduced = true
 			end
 		end
@@ -204,7 +197,9 @@ function GridStatusHealingReduced:UpdateUnit(event, unit)
 
 	local settings
 	if prevented then
-	--	debug("SendStatusGained")
+		reduced = false -- don't show both at once
+
+		self:Debug("SendStatusGained alert_healingPrevented")
 		settings = self.db.profile.alert_healingPrevented
 		self.core:SendStatusGained(UnitGUID(unit), "alert_healingPrevented",
 			settings.priority,
@@ -217,12 +212,12 @@ function GridStatusHealingReduced:UpdateUnit(event, unit)
 			expiration and (expiration - duration),
 			count)
 	else
-	--	debug("SendStatusLost")
+		--self:Debug("SendStatusLost alert_healingPrevented")
 		self.core:SendStatusLost(UnitGUID(unit), "alert_healingPrevented")
 	end
 
 	if reduced then
-	--	debug("SendStatusGained")
+		self:Debug("SendStatusGained: alert_healingReduced")
 		settings = self.db.profile.alert_healingReduced
 		self.core:SendStatusGained(UnitGUID(unit), "alert_healingReduced",
 			settings.priority,
@@ -235,8 +230,17 @@ function GridStatusHealingReduced:UpdateUnit(event, unit)
 			expiration and (expiration - duration),
 			count)
 	else
-	--	debug("SendStatusLost")
+		--self:Debug("SendStatusGained: alert_healingReduced")
 		self.core:SendStatusLost(UnitGUID(unit), "alert_healingReduced")
+	end
+end
+
+function GridStatusHealingReduced:UpdateAllUnits()
+	self:Debug("UpdateAllUnits")
+	if enabled > 0 then
+		for guid, unitid in Grid:GetModule("GridRoster"):IterateRoster() do
+			self:UpdateUnit("UpdateAllUnits", unitid)
+		end
 	end
 end
 
