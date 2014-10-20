221d113cf8443af063b1465b791881a737c3c12a
Phanx <addons@phanx.net>
2014-10-20 10:41:37 +0000

    - Data update - Add print on login listing spell IDs that are no longer valid and requesting users to report them

--- Data.lua
+++ Data.lua
@@ -27,6 +27,7 @@ ns.ReductionDebuffs = {
 	40599,  -- Arcing Smash (Gurtogg Bloodboil in Black Temple)
 	41478,  -- Dampen Magic (High Nethermancer Zerevor in Black Temple)
 	45347,  -- Dark Touched (Lady Sacrolash in Sunwell Plateau)
+	45996,  -- Darkness (M’uru in Sunwell Plateau)
 	38572,  -- Mortal Cleave (Vashj'ir Honor Guard in Serpentshrine Cavern)
 	29572,  -- Mortal Strike (Lord Robin Daris in Karazhan)
 	46296,  -- Necrotic Poison (Cataclysm Hound in Sunwell Plateau)
@@ -100,6 +101,7 @@ ns.ReductionDebuffs = {
 
 	--[[ CATACLYSM - RAID ]]
 	100526, -- Blistering Wound (Flamewaker Subjugator in Firelands)
+	98443,  -- Fiery Cyclone (Majordomo Staghelm in Firelands)
 	83908,  -- Malevolent Strikes (Halfus Wyrmbreaker in The Bastion of Twilight)
 	103002, -- Mortal Strike (Twilight Bruiser in Hour of Twilight)
 	80390,  -- Mortal Strike (Drakonid Slayer in Blackwing Descent)
@@ -111,24 +113,27 @@ ns.ReductionDebuffs = {
 	--[[ CATACLYSM - PARTY ]]
 	76189,  -- Crepuscular Veil (Shadow of Obsidius in Blackrock Caverns)
 	93956,  -- Cursed Veil (Baron Silverlaine in Shadowfang Keep)
+	88010,  -- Cyclone (Wild Vortex in Vortex Pinnacle)
 	91801,  -- Mortal Strike (Wailing Guardsman in Shadowfang Keep)
 	93675,  -- Mortal Wound (Lord Godfrey in Shadowfang Keep)
+	91099,  -- Rotting Bile (Corpse Eater in Shadowfang Keep)
 	23224,  -- Veil of Shadow (Baron Silverlaine in Shadowfang Keep)
 	83926,  -- Veil of Shadow (Vicious Mindlasher in Throne of the Tides)
 	75571,  -- Wounding Strike (Rom'ogg Bonecrusher in Blackrock Caverns)
 
 	--[[ CATACLYSM - SOLO ]]
 	86816,  -- Giant's Bane (Dragul Giantbutcher in Deepholm)
+	93082,  -- Nerfbat (Ghostcrawler in Abyssal Depths)
 
 	--[[ MISTS OF PANDARIA - RAID ]]
 	148375, -- Brutal Hemorrhage (Gate-Crasher Sav'ah in Siege of Orgrimmar)
+	117949, -- Closed Circuit (Celestial Protector in Mogu’shan Vaults)
 	126901, -- Mortal Rend (Kor'thik Fleshrender in Heart of Fear)
 	136670, -- Mortal Strike (Drakkari Frozen Warlord in Throne of Thunder)
 	115804, -- Mortal Wounds (Sha of Anger, monk, warrior)
 	139550, -- Torment (Tormented Spirit, Throne of Thunder)
 	140049, -- Wounding Strike (Zandalari Blade Initiate in Throne of Thunder)
 	144056, -- Xuen's Presence: Mortal Strike
-	144063, -- Xuen's Presence: Standard Raid Debuffs
 
 	--[[ MISTS OF PANDARIA - PARTY ]]
 	133001, -- Gas Bomb (Rak'gor Bloodrazor in Dagger in the Dark)
@@ -144,16 +149,26 @@ ns.ReductionDebuffs = {
 	111826, -- Ancient Poison (Krasari Tormentor in Krasarang Wilds)
 	139010, -- Darkest Darkness (Mumta in Isle of Thunder)
 	126195, -- Dirty Mouth (Vicejaw in Vale of Eternal Blossoms)
+	122078, -- Fear of Darkness (Dreadling in Townlong Steppes)
 	129564, -- Fiery Strike (Bataari Fire-Warrior in Kun-Lai Summit)
 	125353, -- Rank Bite (turtles in Kun-Lai Summit; Townlong Steppes)
 	128964, -- Rune of Suffering (Krasari Runekeeper in Krasarang Wilds)
 
+	--[[ WARLORDS OF DRAENOR - RAID ]]
+
+	--[[ WARLORDS OF DRAENOR - PARTY ]]
+
+	--[[ WARLORDS OF DRAENOR - SOLO ]]
+	170221, -- Elemental Fragment (conjured item)
+	170466, -- Shadow Slash (Lady Temptessa in Shadowmoon Valley)
+
 	--[[ PLAYERS ]]
-	30213,  -- Legion Strike (warlock felguard)
-	54680,  -- Monstrous Bite (hunter devilsaur)
-	115625, -- Mortal Cleave (warlock wrathguard)
-	82654,  -- Widow Venom (hunter)
-	8680,   -- Wound Poison (rogue)
+	33786,  -- Cyclone (Druid)
+	30213,  -- Legion Strike (Warlock Felguard)
+	54680,  -- Monstrous Bite (Hunter Devilsaur)
+	115625, -- Mortal Cleave (Warlock Wrathguard)
+	12294,  -- Mortal Strike (Warior, Arms)
+	8680,   -- Wound Poison (Rogue)
 
 	--[[ PVP NPCs ]]
 	76727,  -- Mortal Strike (Twilight Armsmaster in Grim Batol)
@@ -185,19 +200,35 @@ ns.ReductionDebuffs = {
 	36974,  -- Wound Poison
 
 	--[[ UNKNOWN ]]
+	161242, -- Caustic Energy WOD?
+	166766, -- Crippling Blow WOD?
+	164479, -- Festering Wound WOD?
+	174439, -- Fierce Bludgeoning WOD?
+	174564, -- Fungal Burst WOD?
+	175624, -- Grievous Mortal Wounds WOD?
+	173240, -- Grievous Strike WOD?
+	171527, -- Infected Wound WOD?
 	39595,  -- Mortal Cleave
+	177147, -- Mortal Cleave WOD?
 	61042,  -- Mortal Smash
 	40220,  -- Mortal Strike
 	120436, -- Mortal Strike
-	97857,  -- Phoenix Flame
+	164457, -- Mortal Strike WOD?
+	164466, -- Mortal Strike WOD?
+	172769, -- Mortal Strike WOD?
+	177883, -- Mortal Strike WOD?
 	135531, -- Reforged Soul
 	133767, -- Serious Wound
+	170567, -- Shadow Spike WOD?
+	172941, -- Smash WOD?
 	60084,  -- The Veil of Shadows
 	115195, -- Toxic Shock
 	68881,  -- Unstable Water Nova
-	142679, -- Veil of Darkness
 	24674,  -- Veil of Shadow
+	155586, -- Veil of Shadow WOD?
+	174811, -- Void Expulsion WOD?
 	136577, -- Wind Storm
+	178050, -- Wound Poison WOD?
 }
 
 ns.PreventionDebuffs = {
@@ -224,4 +255,28 @@ ns.PreventionDebuffs = {
 	--[[ MISTS OF PANDARIA - RAID ]]
 	114078, -- HUGE, SHARP TEETH! (Darkmoon Rabbit in Darkmoon Island)
 	148386, -- Black Ash (item on Timeless Isle)
+
+	--[[ MISTS OF PANDARIA - PARTY]]
+	127280, -- Viletongue Sting (Huntmaster S’thoc and Viletongue Skirmisher in A Brewing Storm)
+
+	--[[ WARLORDS OF DRAENOR - SOLO ]]
+	172624, -- Shrieking Winds (Kalandrios in Nagrand)
+	176064, -- Sinister Spores (item)
+	172622, -- Torrential Waters (Aborius in Nagrand)
+	176059, -- Unleashed Mania (item)
+	172618, -- Unrelenting Flames (Incerinatus in Nagrand)
+
+	--[[ OTHER ]]
+	172039, -- Luchador Costume (Day of the Dead holiday event costumes)
+
+	--[[ UNKNOWN ]]
+	145862, -- Banish WOD?
+	160209, -- Cyclone WOD?
+	84599,  -- Engulfed
+	79335,  -- Frozen Solid
+	159219, -- Necrotic Breath WOD?
+	152773, -- Possession WOD?
+	84597,  -- Rocked
+	123648, -- Shock
+
 }
